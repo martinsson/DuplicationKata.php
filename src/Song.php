ddb31faea39963f0dba5315e075934850f622fc4
raaaahman <sylvain@devindetails.com>
2020-03-30 16:43:07 +0200

    Lesson2Variable

--- src/Song.php
+++ src/Song.php
@@ -3,6 +3,8 @@
 class Song
 {
     var $song;
+    private $startingLine;
+    private $songPattern;
 
     public function __construct()
     {
@@ -14,9 +16,27 @@ class Song
         $this->song .= $line . "\n";
     }
 
-    public function singFullSong($fullSong) {
-        foreach (preg_split('/^.*$/', $fullSong ) as $line) {
+    public function singFullSong($fullSong)
+    {
+        foreach (preg_split('/\n/', $fullSong) as $line) {
             $this->sing($line);
+        }
+    }
+
+    public function singPattern($songPattern)
+    {
+        $this->songPattern = $songPattern;
+        return $this;
+    }
+
+    public function fromLineNb($lineNb) {
+        $this->startingLine = $lineNb;
+        return $this;
+    }
+    public function toLineNb($lineNb) {
+        $step = $this->startingLine - $lineNb > 0 ? -1 : 1;
+        for($currentLine = $this->startingLine; $currentLine != $lineNb + $step; $currentLine += $step) {
+            $this->singFullSong(sprintf( $this->songPattern, $currentLine, $currentLine + $step ));
         }
     }
 }
