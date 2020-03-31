301a257e43a6b265dd770e03a9f1e52d4b42b889
raaaahman <sylvain@devindetails.com>
2020-03-31 16:33:34 +0200

    Extract factory method for SongStyles

--- src/Lesson21.php
+++ src/Lesson21.php
@@ -4,36 +4,46 @@ class Lesson21 extends Song
 {
   public function singSong($style, $names)
   {
-    $songStyles = [
-        SongStyle::create()
-            ->registerSpecialLine("Hip Hip Horray! For %s")
-            ->when(
-                function($name) {
-                    return strpos($name, "L") === 0;
-                }
-            ),
-        SongStyle::create()
-            ->registerSpecialLine("Say yeah! Say yo! Say %s")
-            ->when(
-                function($name) {
-                    return strpos($name, "am") === 1;
-                }
-            ),
-        SongStyle::create()
-    ];
-
-    $songStyle = $songStyles[$style-1];
-    foreach ($names as $name)
-    {
-      if ($songStyle->hasSpecialLineFor($name))
-      {
-        $this->sing( $songStyle->saySpecialLineFor($name) );
-      }
-      else
-      {
-        $this->sing("Hello " . $name . ", it's nice to meet you.");
-      }
-    }
+      $songStyle = $this->composeSongStyle($style);
+      foreach ($names as $name)
+        {
+          if ($songStyle->hasSpecialLineFor($name))
+          {
+            $this->sing( $songStyle->saySpecialLineFor($name) );
+          }
+          else
+          {
+            $this->sing("Hello " . $name . ", it's nice to meet you.");
+          }
+        }
   }
+
+    /**
+     * @param $style
+     * @return mixed
+     */
+    public function composeSongStyle($style)
+    {
+        $songStyles = [
+            SongStyle::create()
+                ->registerSpecialLine("Hip Hip Horray! For %s")
+                ->when(
+                    function ($name) {
+                        return strpos($name, "L") === 0;
+                    }
+                ),
+            SongStyle::create()
+                ->registerSpecialLine("Say yeah! Say yo! Say %s")
+                ->when(
+                    function ($name) {
+                        return strpos($name, "am") === 1;
+                    }
+                ),
+            SongStyle::create()
+        ];
+
+        $songStyle = $songStyles[$style - 1];
+        return $songStyle;
+    }
 
 }
